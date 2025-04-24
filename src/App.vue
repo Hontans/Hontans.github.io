a2a5492a4113984191e32072f924a98675446753
Hontans <hontanssylvain@gmail.com>
2025-04-24 21:41:03 +0200

    feat: simplify App.vue by removing HelloWorld component and unused assets

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-red-500">salut {{ firstName.toUpperCase() }}</h1>
+  <p>biens vue</p>
 </template>
+
+<script setup>
+  const firstName = "sylvain"
+</script>
