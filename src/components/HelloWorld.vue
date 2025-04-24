070d93078c716116539181302ba61a426c539003
Hontans <hontanssylvain@gmail.com>
2025-04-24 17:19:24 +0200

    feat: update styles and structure in App.vue, HelloWorld.vue, and index.html; configure Tailwind CSS content paths

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,7 +10,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" class="border px-2 py-1" @click="count++">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
